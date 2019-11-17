85589aec9a7f993647b4634155eeed624f06413d
John Azzaro <jazzaro@temple.edu>
2019-11-16 20:33:56 -0800

    added basic server

--- server.js
+++ server.js
@@ -3,3 +3,15 @@
 const express = require('express');
 
 const app = express();
+
+
+app.get('/', function(req, res) {
+    res.send('this app works!');
+});
+
+
+let server;
+
+server = app.listen('3000', function() {
+    console.log('Your app is listening on port 3000...');
+});
