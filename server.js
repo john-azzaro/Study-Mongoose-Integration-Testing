86bff715a884775cd0c413fe3d397f7858e35d95
John Azzaro <jazzaro@temple.edu>
2019-11-20 16:56:58 -0800

    updated runServer, added closeServer

--- server.js
+++ server.js
@@ -18,25 +18,37 @@ const app = express();
 
 let server;
 
-function runServer(databaseUrl, port=PORT) {
-    return new Promise( function(resolve, reject) {
-        mongoose.connect(databaseUrl, { useNewUrlParser: true,  useUnifiedTopology: true }, function() {
-            if (err) {
-                return reject(err);
-            }
-
-            server = app.listen(port, function() {
+function runServer(databaseUrl, port = PORT) {
+    return new Promise(function(resolve, reject) {
+       mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+          if (err) {
+             return reject(err);
+          }
+ 
+          server = app
+             .listen(port, function() {
                 console.log(`Listening on port ${port}...`);
                 resolve();
-            })
-            .on('error', function() {
+             })
+ 
+             .on('error', function(err) {
                 mongoose.disconnect();
-                reject(err); 
-            });
-        });
+                reject(err);
+             });
+       });
     });
-}  
+ }
 
-server = app.listen('3000', function() {
-    console.log('Your app is listening on port 3000...');
-});
+ function closeServer() {
+    return mongoose.disconnect().then(function() {
+       return new Promise(function(resolve, reject) {
+          console.log('Closing server');
+          server.close(function(err) {
+             if (err) {
+                return reject(err);
+             }
+             resolve();
+          });
+       });
+    });
+ }
