d4f889792eaedbd2737124c7734fd2b3683f1ed9
John Azzaro <jazzaro@temple.edu>
2019-11-20 16:54:05 -0800

    added run Server function

--- server.js
+++ server.js
@@ -5,16 +5,37 @@ const express = require('express');
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise();
 
+
+// Module Imports
+const {DATABASE_URL, PORT} = require('./config.js');
+
+
 // Instantiation
 const app = express();
-
-
-
 
 
 // Server
 
 let server;
+
+function runServer(databaseUrl, port=PORT) {
+    return new Promise( function(resolve, reject) {
+        mongoose.connect(databaseUrl, { useNewUrlParser: true,  useUnifiedTopology: true }, function() {
+            if (err) {
+                return reject(err);
+            }
+
+            server = app.listen(port, function() {
+                console.log(`Listening on port ${port}...`);
+                resolve();
+            })
+            .on('error', function() {
+                mongoose.disconnect();
+                reject(err); 
+            });
+        });
+    });
+}  
 
 server = app.listen('3000', function() {
     console.log('Your app is listening on port 3000...');
