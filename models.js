a5c03a1ae614fe6165ca6c4b302fd763f1814a42
John Azzaro <jazzaro@temple.edu>
2019-11-21 16:24:43 -0800

    added schema

--- models.js
+++ models.js
@@ -1,4 +1,25 @@
 "use strict";
 
+// Imports
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const restaurantSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  borough: { type: String, required: true },
+  cuisine: { type: String, required: true },
+  address: {
+      building: String,
+      coord: [String],
+      street: String,
+      zipcode: String
+  },
+  grades: [
+      {
+      date: Date,
+      grade: String,
+      score: Number
+      }
+  ]
+});
 
