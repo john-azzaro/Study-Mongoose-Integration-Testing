950ac68b91d0b0f717e90c66e949702c997c08ec
John Azzaro <jazzaro@temple.edu>
2019-11-21 18:05:32 -0800

    updated models, add grade virtual

--- models.js
+++ models.js
@@ -4,6 +4,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
+// Schemas
 const restaurantSchema = mongoose.Schema({
   name: { type: String, required: true },
   borough: { type: String, required: true },
@@ -23,10 +24,24 @@ const restaurantSchema = mongoose.Schema({
   ]
 });
 
+// Virtuals
 restaurantSchema.virtual('addressString').get(function() {
   return `${this.address.building} ${this.address.street}`.trim();
 });
 
+restaurantSchema.virtual("grade").get(function() {
+  const gradeObj =
+    this.grades.sort((a, b) => {
+      return b.date - a.date;
+    })[0] || {};
+  return gradeObj.grade;
+});
+
+// Instance methods
+
+
+// Models
 const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
+// Exported Models
 module.exports = { Restaurant }
