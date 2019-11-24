96130fa43be010ff59e1be971610ecee62fea9cd
John Azzaro <jazzaro@temple.edu>
2019-11-23 22:27:17 -0800

    added put rerquest to server

--- server.js
+++ server.js
@@ -77,7 +77,27 @@ app.post('/restaurants', (req, res) => {
 
 
 app.put('/restaurants/:id', (req, res) => {
+   if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+      const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+      console.error(message);
+      return res.status(400).json({ message: message });
+  }
 
+  const toUpdate = {};
+  const updateableFields = ['name', 'borough', 'cuisine', 'address'];
+
+  updateableFields.forEach(field => {
+      if (field in req.body) {
+      toUpdate[field] = req.body[field];
+      }
+  });
+
+  Restaurant
+      .findByIdAndUpdate(req.params.id, { $set: toUpdate })
+      .then(restaurant => res.status(204).end())
+      .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
 
