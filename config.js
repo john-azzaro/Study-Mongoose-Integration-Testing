3597d8255129fe8eb17de0d511722976ccab90e8
John Azzaro <jazzaro@temple.edu>
2019-11-26 11:23:02 -0800

    added database notes

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+//Database
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
+//Port
 exports.PORT = process.env.PORT || 3000;
