1a896330b3e1eb284f876f9d996398d354e87cc4
John Azzaro <jazzaro@temple.edu>
2019-11-25 17:06:10 -0800

    adding integration tests

--- server.js
+++ server.js
@@ -155,3 +155,5 @@ function closeServer() {
 if (require.main === module) { 
     runServer(DATABASE_URL).catch(err => console.error(err));
 }
+
+// module.exports = { app, runServer, closeServer };
